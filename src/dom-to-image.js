86c684513ffa27e6a527d16727ff20142604423f
zrajm <zrajm@klingonska.org>
2018-10-26 02:09:47 +0200

    Fixes tsayen/dom-to-image#251: Don't double encode
    '#' and line feed (\x0A) where URI encoded twice, resulting in '%23' and '%0A'
    remaining after string decoding. It seems Firefox preseve line feeds in CSS in
    some places, which caused some CSS instructions to be ignored.
    
    Also, the removed URI encoding function was, counterintuitively, called
    `escapeXhtml`.

--- src/dom-to-image.js
+++ src/dom-to-image.js
@@ -332,7 +332,6 @@
                 node.setAttribute('xmlns', 'http://www.w3.org/1999/xhtml');
                 return new XMLSerializer().serializeToString(node);
             })
-            .then(util.escapeXhtml)
             .then(function (xhtml) {
                 return '<foreignObject x="0" y="0" width="100%" height="100%">' + xhtml + '</foreignObject>';
             })
@@ -358,7 +357,6 @@
             uid: uid(),
             delay: delay,
             asArray: asArray,
-            escapeXhtml: escapeXhtml,
             makeImage: makeImage,
             width: width,
             height: height
@@ -545,10 +543,6 @@
             var length = arrayLike.length;
             for (var i = 0; i < length; i++) array.push(arrayLike[i]);
             return array;
-        }
-
-        function escapeXhtml(string) {
-            return string.replace(/#/g, '%23').replace(/\n/g, '%0A');
         }
 
         function width(node) {
